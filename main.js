81842b26442fac03591d51f61fb5888be2b832ec
Evgeni Kolev <ekoleww@gmail.com>
2019-04-01 12:16:55 +0300

    MS Edge scroll animations

--- main.js
+++ main.js
@@ -48,103 +48,23 @@ var burgerSpan = document.querySelectorAll('.bg-span');
 
 
 
+// jQuery scroll animations 
 
-
-
-//smooth scroll NAV BAR
-
-function smoothScroll(target, duration){
-
-    var target = document.querySelector(target);
-    var targetPosition = target.getBoundingClientRect().top;
-    var startPosition = window.pageYOffset;
-    var distance = targetPosition - startPosition;
-    var startTime = null;
-
-   
-    function animation(currentTime){
-
-    if (startTime === null) 
-       startTime = currentTime;   
-      
-     var timeElapsed = currentTime - startTime; 
-     var run = ease(timeElapsed, startPosition, distance, duration);
-     window.scrollTo(0,run);
-     if (timeElapsed < duration) 
-        requestAnimationFrame(animation);
-    }
-
-
-    function ease(t, b, c, d) {
-        t /= d/2;
-        if (t < 1) return c/2*t*t + b;
-        t--;
-        return -c/2 * (t*(t-2) - 1) + b;
-    }
-
-     
-   requestAnimationFrame(animation); 
-
-}
-//attaching scroll animations 
-
-const navAbout = document.querySelector('.nav-about');
-const navMenu = document.querySelector('.nav-menu');
-const navContact = document.querySelector('.nav-contact');
-
-
-
-navAbout.addEventListener('click',function(){
-    mainNav.animate([
-        { top: '0px' }, 
-        { top: '-100px' }
-      ], {
-        duration: 0500,
-        fill: 'forwards'
-      });
-
-      for(var i = 0; i < burgerSpan.length; i++){
-        burgerSpan[i].style.backgroundColor = "#EDF5E1";
-      }
-      navBurger.classList.remove("open");
-    smoothScroll('.about',1000);
+$('.nav-about').click(function(e){
+  e.preventDefault();
+  $('html, body').animate({
+      scrollTop: $('.about').offset().top
+  }, 700);
 });
 
-navMenu.addEventListener('click',function(){
-    mainNav.animate([
-        { top: '0px' }, 
-        { top: '-100px' }
-      ], {
-        duration: 0500,
-        fill: 'forwards'
-      });
-
-      for(var i = 0; i < burgerSpan.length; i++){
-        burgerSpan[i].style.backgroundColor = "#EDF5E1";
-      }
-
-      navBurger.classList.remove("open");
-    smoothScroll('.about-coffee',1000);
+$('.nav-menu').click(function(e){
+  e.preventDefault();
+  $('html, body').animate({
+      scrollTop: $('.about-coffee').offset().top
+  }, 700);
 });
 
-navContact.addEventListener('click', function(){
-    mainNav.animate([
-        { top: '0px' }, 
-        { top: '-100px' }
-      ], {
-        duration: 0500,
-        fill: 'forwards'
-      });
-
-      for(var i = 0; i < burgerSpan.length; i++){
-        burgerSpan[i].style.backgroundColor = "#EDF5E1";
-      }
-
-      navBurger.classList.remove("open");
-});
-
-
-// JQUERY  for arrow and buttons           
+// jQuery  for arrow and buttons           
 
 $('.arrow-landing').click(function(e){
     e.preventDefault();
